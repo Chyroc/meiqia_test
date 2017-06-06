becbac8dc1e01559e09a4ae947d9a7e1ab0c458a
Chyroc <chen_yunpeng@foxmail.com>
2017-06-06 17:59:14 +0800

    get all nba players name

--- hupu/hupu/spiders/players.py
+++ hupu/hupu/spiders/players.py
@@ -8,8 +8,13 @@ class PlayersSpider(scrapy.Spider):
     start_urls = ['https://nba.hupu.com/players']
 
     def parse_players(self, response):
-        pass
-        # TODO
+        info = response.xpath('//table[@class="players_table"]/tbody/tr/td[2]')
+        name_cn = info.xpath('b/a/text()').extract()
+        name_en = info.xpath('p/b/text()').extract()
+        with open('name_word.txt', 'a', errors='ignore') as f:
+            for i in zip(name_cn, name_en):
+                f.write('{} {}'.format(*i))
+                f.write('\n')
 
     def parse(self, response):
         teams_url = response.xpath('//ul[@class="players_list"]/li/span/a/@href').extract()
