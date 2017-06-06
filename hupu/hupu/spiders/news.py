3371abe2b1398fb55e3bb62f8f24ce0826b035ae
Chyroc <chen_yunpeng@foxmail.com>
2017-06-06 17:40:38 +0800

    add time

--- hupu/hupu/spiders/news.py
+++ hupu/hupu/spiders/news.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
 import scrapy
 
 
@@ -12,6 +14,18 @@ class NewsSpider(scrapy.Spider):
         url = urls[1] if len(urls) == 2 else urls[0]
         return response.urljoin(url)
 
+    def is_time_before_30_days(self, news_time):
+        time_cmp = datetime.datetime.now() - datetime.datetime.strptime(news_time, '%Y-%m-%d %H:%M')
+        return time_cmp.days > 30
+
+    def get_time(self, response):
+        # TODO
+        news_time = response.xpath('//div[@class="news-list"]/ul/li[1]/div[2]/span[1]/a/@title').extract()[0]
+        return news_time
+
     def parse(self, response):
-        next_page = self.get_next_page(response)
-        yield scrapy.Request(next_page, callback=self.parse)
+        news_time = self.get_time(response)
+
+        if not self.is_time_before_30_days(news_time):
+            next_page = self.get_next_page(response)
+            yield scrapy.Request(next_page, callback=self.parse)
