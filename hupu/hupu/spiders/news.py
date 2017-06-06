ebd7338e9b2c106935542ced5acdb7e5b72fee7c
Chyroc <chen_yunpeng@foxmail.com>
2017-06-06 17:42:07 +0800

    add get content url list

--- hupu/hupu/spiders/news.py
+++ hupu/hupu/spiders/news.py
@@ -18,13 +18,20 @@ class NewsSpider(scrapy.Spider):
         time_cmp = datetime.datetime.now() - datetime.datetime.strptime(news_time, '%Y-%m-%d %H:%M')
         return time_cmp.days > 30
 
-    def get_time(self, response):
-        # TODO
+    def get_title_url_time(self, response):
+        news_list = response.xpath('//div[@class="news-list"]/ul/li')
+        urls = filter(lambda k: k is not None, [i.xpath('div[1]/h4/a/@href').extract_first() for i in news_list])
         news_time = response.xpath('//div[@class="news-list"]/ul/li[1]/div[2]/span[1]/a/@title').extract()[0]
-        return news_time
+        return urls, news_time
+
+    def parse_content(self, response):
+        # TODO
+        pass
 
     def parse(self, response):
-        news_time = self.get_time(response)
+        urls, news_time = self.get_title_url_time(response)
+        for i in urls:
+            yield scrapy.Request(i, callback=self.parse_content)
 
         if not self.is_time_before_30_days(news_time):
             next_page = self.get_next_page(response)
