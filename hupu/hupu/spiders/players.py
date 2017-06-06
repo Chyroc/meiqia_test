87681c7454c882f1ef66e92a7b7f7390cdb0658f
Chyroc <chen_yunpeng@foxmail.com>
2017-06-06 18:03:35 +0800

    Players scrapy (#3)
    * get all nba teams url
    
    * get all nba players name
    
    * fix encoding

---
+++ hupu/hupu/spiders/players.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+
+class PlayersSpider(scrapy.Spider):
+    name = 'players'
+    start_urls = ['https://nba.hupu.com/players']
+
+    def parse_players(self, response):
+        info = response.xpath('//table[@class="players_table"]/tbody/tr/td[2]')
+        name_cn = info.xpath('b/a/text()').extract()
+        name_en = info.xpath('p/b/text()').extract()
+        with open('name_word.txt', 'a', errors='ignore', encoding='utf-8') as f:
+            for i in zip(name_cn, name_en):
+                f.write('{} {}'.format(*i))
+                f.write('\n')
+
+    def parse(self, response):
+        teams_url = response.xpath('//ul[@class="players_list"]/li/span/a/@href').extract()
+
+        for i in teams_url:
+            yield scrapy.Request(i, callback=self.parse_players)
