b72ffad84643a158a97a53fde97cf6447bc7b0ea
Chyroc <chen_yunpeng@foxmail.com>
2017-06-06 17:45:29 +0800

    save content

--- hupu/hupu/pipelines.py
+++ hupu/hupu/pipelines.py
@@ -5,7 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
 
-class HupuPipeline(object):
+
+class SaveContentPipeline(object):
     def process_item(self, item, spider):
+        title = item['title']
+        content = item['content']
+        time = item['time']
+
+        if not os.path.exists('data'):
+            os.mkdir('data')
+
+        with open('data/[{}]{}.txt'.format(time.replace(':', '-'), title), 'w', errors='ignore') as f:
+            f.write(' '.join([title, content]))
         return item
